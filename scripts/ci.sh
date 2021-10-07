d76c65b15bd668be0d9797deb01b1a9ae7a4253f
Stuart Leeks <stuart@leeks.net>
2021-10-07 08:32:08 +0000

    Change to avoid zip-within-zip in artifact

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,14 +3,15 @@ set -e
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+rm -rf "$script_dir/../build-output"
 mkdir -p "$script_dir/../build-output"
 
 figlet Functions
 cd functions
 yarn
 
-zip -r "$script_dir/../build-output/functions.zip" . -x local.*
-
+cp -R . "$script_dir/../build-output/functions"
+rm -rf "$script_dir/../build-output/functions/local.*"
 
 if [[ -z $IS_CI ]]; then
     echo "IS_CI not set, skipping git status check"
